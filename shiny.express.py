64b6ba911aac02ab5d1f7b651b78fc4697ea6dd1
bruno-8km <bruno_ocd_iap@hotmail.com>
2025-05-29 20:47:40 -0300

    Add files via upload

--- shiny.express.py
+++ shiny.express.py
@@ -55,7 +55,7 @@ def get_bmi_classification_value(imc):
 
 # Carrega dados de saúde personalizados
 #df = pd.read_csv("https://raw.githubusercontent.com/bruno-8km/meuprojeto/master/points_per_health.csv")
-df = pd.read_csv("https://github.com/bruno-8km/shiny/blob/master/points_per_health.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/bruno-8km/shiny/refs/heads/master/points_per_health.csv")
 
 # Adiciona colunas de exemplo 'PAS', 'PAD', e 'IMC'
 if 'PAS' not in df.columns or 'PAD' not in df.columns:
